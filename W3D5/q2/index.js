bec6dc982a8ee0da08235aeba39a65e4f71bbd3f
eyobmbt <eyobmbt@gmail.com>
2021-10-15 22:50:09 -0500

    W3D4 Assignment
    Assignment

---
+++ W3D5/q2/index.js
@@ -0,0 +1,27 @@
+/*GET and POST: In your q2 directory be sure to start with npm init and install express. Your
+index.js application for this exercise should return a form similar to what is shown below for
+GET requests to ‘/’.
+The form should submit with method POST to “/result”, and then output a simple string similar
+to the output of question1. Hint: you’ll need to use the express.urlencoded middleware so that
+the post parameters are parsed on req.body.
+Return output after POST (don’t redirect yet in this exercise). Try and see what happens when
+you bookmark the result page (and then use the bookmark).*/
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded());
+app.get('/',(req,res)=>{
+    let form=req.query.form;
+     form=`<form action="/result" method="post">
+    Name: <input name="name">
+    Age: <input name="age"> <button type="submit">
+    Submit</button></form>`;
+    res.send(form);
+});
+ app.post('/result',(req,res)=>{
+    let name = req.body.name;
+     let age=req.body.age;
+    res.send(`Welcome ${name} and Your age is: ${age}`);
+    
+ });
+app.listen(3003);
---
+++ W3D5/q4/index.js
@@ -0,0 +1,52 @@
+
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+const path=require('path');
+app.use(bodyParser.urlencoded());
+app.use('/css', express.static(path.join(__dirname, 'css')));
+const date_ob = new Date();
+const hour = date_ob.getHours();
+app.get('/',(req,res)=>{
+    let href='css/night.css';
+    if(hour>=6 && hour<=18){
+        href='css/day.css';
+    }
+    let form=req.query.form;
+     form=`
+     <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link href="${href}" rel="stylesheet"/>
+        <title>W3D5 Assignment</title>
+
+    </head>
+    <body>
+        <h2>W3D5 Assignment-Express Get/Post form</h2>
+        <h2>Day: ${date_ob} Hour: ${hour}</h2>
+     <form action="/result" method="get">
+    Name: <input name="name">
+    Age: <input name="age"> <button type="submit">
+    Submit</button></form>
+    </body>
+    </html>`;
+    res.send(form);
+ 
+});
+
+
+ app.get('/result',(req,res)=>{
+
+    let name = req.query.name;
+     let age=req.query.age;
+    
+res.send(`Welcome ${name} and Your age is: ${age} `);
+   
+ });
+ app.get('/output',(req,res)=>{
+
+ res.redirect(303,'/redirect');
+ 
+ });
+app.listen(3005);
